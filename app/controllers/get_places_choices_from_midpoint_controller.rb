36b359dbf9d34026010d429607648f99288fbe6d
poojarat <48258399+poojarat@users.noreply.github.com>
2019-08-15 14:38:23 -0400

    Fixed route change and refactored Bearer Token
    Bearer token is now a variable that can be adjusted if we need

--- app/controllers/get_places_choices_from_midpoint_controller.rb
+++ app/controllers/get_places_choices_from_midpoint_controller.rb
@@ -1,9 +1,11 @@
 class GetPlacesChoicesFromMidpointController < ApplicationController
   require 'HTTParty'
-  def foo
+  def yelp
     lat, lon  = params['lat'], params['lon']
-    response = HTTParty.get("https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}",headers: {"Authorization" => "Bearer GXHILCW8iiPEDUbfYwUXO2J5-CLmd4LHj0jjNYJPg-8eUTtISLw8eXedGhzH_6eFj7cy8sfYAH-5QvT8r6zF9JVybTsiFOq7FiDCzUwVAqRSOuvL9KfAIlAfAaRMXXYx"}) 
+    url = "https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}"
+    env_bearer_token = "GXHILCW8iiPEDUbfYwUXO2J5-CLmd4LHj0jjNYJPg-8eUTtISLw8eXedGhzH_6eFj7cy8sfYAH-5QvT8r6zF9JVybTsiFOq7FiDCzUwVAqRSOuvL9KfAIlAfAaRMXXYx"
+    response = HTTParty.get("#{url}",headers: {"Authorization" => "Bearer #{env_bearer_token}"}) 
     # next step is to refactor
-    render json: [{lat: lat, lon: lon, data: response.parsed_response}]
+    render json: [{data: response.parsed_response}]
   end
 end
