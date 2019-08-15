2eef63a9ba1eff2a40499ac489233416d9aadf98
poojarat <48258399+poojarat@users.noreply.github.com>
2019-08-15 14:23:19 -0400

    Changed foo to more understandable yelp

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :cities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
-  get 'places_choices/', to: 'get_places_choices_from_midpoint#foo'
+  get 'places_choices/', to: 'get_places_choices_from_midpoint#yelp'
 end
