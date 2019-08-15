21bbd07b7bee8041a535c9b5e2eaf94ea6335b33
poojarat <48258399+poojarat@users.noreply.github.com>
2019-08-15 14:45:35 -0400

    ENV variable for Bearer token

--- app/controllers/get_places_choices_from_midpoint_controller.rb
+++ app/controllers/get_places_choices_from_midpoint_controller.rb
@@ -3,8 +3,7 @@ class GetPlacesChoicesFromMidpointController < ApplicationController
   def yelp
     lat, lon  = params['lat'], params['lon']
     url = "https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}"
-    env_bearer_token = "GXHILCW8iiPEDUbfYwUXO2J5-CLmd4LHj0jjNYJPg-8eUTtISLw8eXedGhzH_6eFj7cy8sfYAH-5QvT8r6zF9JVybTsiFOq7FiDCzUwVAqRSOuvL9KfAIlAfAaRMXXYx"
-    response = HTTParty.get("#{url}",headers: {"Authorization" => "Bearer #{env_bearer_token}"}) 
+    response = HTTParty.get("#{url}",headers: {"Authorization" => "Bearer #{ENV.fetch('YELP_BEARER_TOKEN')}"}) 
     # next step is to refactor
     render json: [{data: response.parsed_response}]
   end
