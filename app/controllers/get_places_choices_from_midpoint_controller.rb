e73fd2c60bd6380ebc0b708684bbfbb510eccfa5
sophiabanda <51414936+sophiabanda@users.noreply.github.com>
2019-08-15 14:49:27 -0400

    Update get_places_choices_from_midpoint_controller.rb

--- app/controllers/get_places_choices_from_midpoint_controller.rb
+++ app/controllers/get_places_choices_from_midpoint_controller.rb
@@ -4,7 +4,6 @@ class GetPlacesChoicesFromMidpointController < ApplicationController
     lat, lon  = params['lat'], params['lon']
     url = "https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}"
     response = HTTParty.get("#{url}",headers: {"Authorization" => "Bearer #{ENV.fetch('YELP_BEARER_TOKEN')}"}) 
-    # next step is to refactor
     render json: [{data: response.parsed_response}]
   end
 end
