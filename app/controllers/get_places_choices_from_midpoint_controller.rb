fad9ad526ea5bdf3130e42bcd518e267d0ad092f
Tushar Poojara <poojarat@gmail.com>
2019-08-16 12:55:51 -0400

    Initial Deployment

--- app/controllers/get_places_choices_from_midpoint_controller.rb
+++ app/controllers/get_places_choices_from_midpoint_controller.rb
@@ -1,9 +1,8 @@
 class GetPlacesChoicesFromMidpointController < ApplicationController
-  require 'HTTParty'
   def yelp
-    lat, lon, limit  = params['lat'], params['lon'], params['limit']
-    url = "https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}&limit=#{limit}&sort_by=distance"
-    response = HTTParty.get("#{url}",headers: {"Authorization" => "Bearer #{ENV["YELP_BEARER_TOKEN"]}"}) 
+    lat, lon  = params['lat'], params['lon']
+    url = "https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}&sort_by=distance"
+    response = HTTParty.get("#{url}",headers: {"Authorization" => "Bearer #{ENV['YELP_BEARER_TOKEN']}"})
     render json: [{data: response.parsed_response}]
   end
 end
