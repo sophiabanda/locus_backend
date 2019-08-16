e63e2b109df3c10c7638bfbddc401b12b63a92df
poojarat <48258399+poojarat@users.noreply.github.com>
2019-08-16 13:02:36 -0400

    Remove require 'HTTParty'
    It is not needed in Ruby.  It was throwing errors to the front end when calling it.

--- app/controllers/get_places_choices_from_midpoint_controller.rb
+++ app/controllers/get_places_choices_from_midpoint_controller.rb
@@ -1,5 +1,4 @@
 class GetPlacesChoicesFromMidpointController < ApplicationController
-  require 'HTTParty'
   def yelp
     lat, lon, limit  = params['lat'], params['lon'], params['limit']
     url = "https://api.yelp.com/v3/businesses/search?latitude=#{lat}&longitude=#{lon}&limit=#{limit}&sort_by=distance"
